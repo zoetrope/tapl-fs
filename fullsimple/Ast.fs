35293c064f2da742d1fb3755ab2da742c35e6a2d
zoetrope <a.ikezoe@gmail.com>
2013-07-19 20:27:21 +0900

    add fullsimple project

---
+++ fullsimple/Ast.fs
@@ -0,0 +1,45 @@
+﻿[<AutoOpen>]
+module Ast
+
+type ty =
+  | TyVar of int * int
+  | TyId of string
+  | TyArr of ty * ty
+  | TyUnit
+  | TyRecord of (string * ty) list
+  | TyVariant of (string * ty) list
+  | TyBool
+  | TyString
+  | TyFloat
+  | TyNat
+
+type term =
+  | TmTrue
+  | TmFalse
+  | TmIf of term * term * term
+  | TmCase of term * (string * (string * term)) list
+  | TmTag of string * term * ty
+  | TmVar of int * int
+  | TmAbs of string * ty * term
+  | TmApp of term * term
+  | TmLet of string * term * term
+  | TmFix of term
+  | TmString of string
+  | TmUnit
+  | TmAscribe of term * ty
+  | TmRecord of (string * term) list
+  | TmProj of term * string
+  | TmFloat of float
+  | TmTimesfloat of term * term
+  | TmZero
+  | TmSucc of term
+  | TmPred of term
+  | TmIsZero of term
+  | TmInert of ty
+
+type binding =
+  | NameBind
+  | TyVarBind
+  | VarBind of ty
+  | TmAbbBind of term * ty option
+  | TyAbbBind of ty
---
+++ fullsimple/Parser.fs
@@ -0,0 +1,158 @@
+﻿[<AutoOpen>]
+module Parser
+
+open System
+open System.IO
+open FParsec
+open FParsec.Primitives
+open FParsec.CharParsers
+
+let pTrue = (stringReturn "true" TmTrue) .>> spaces
+let pFalse = (stringReturn "false" TmFalse) .>> spaces
+
+let pBoolType = stringReturn "Bool" TyBool
+let pStringType = stringReturn "String" TyString
+let pUnitType = stringReturn "Unit" TyString
+let pFloatType = stringReturn "Float" TyString
+let pNatType = stringReturn "Nat" TyString
+
+let identifier =
+    let isIdentifierFirstChar c = isLetter c || c = '_'
+    let isIdentifierChar c = isLetter c || isDigit c || c = '_'
+    many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier"
+
+
+
+let rec pType = pArrowType
+and pAType = pParenType <|>
+    identifier <|>
+    pBoolType <|>
+    "<" pFieldTypes ">" <|>
+    pStringType <|>
+    pUnitType <|>
+    "{" pFieldTypes "}" <|>
+    pFloatType <|>
+    pNatType
+
+and pArrow = chainr1 pAType (pstring "->" >>% fun x1 x2 -> TyArr(x1, x2))
+
+and pArrowType = attempt(pArrow) <|> pAType
+
+and pParenType = parse{ 
+    let! _ = pstring "("
+    let! t = pType
+    let! _ = pstring ")"
+    return t}
+    
+let rec pTerm = pAppTerm <|> pIf <|> pCaseOfCases <|> pLambda <|> pLet <|> pLetRec
+
+and pCaseOfCases = "case" pTerm pCases
+
+and pLet = parse{
+    let! _ = pstring "let" .>> spaces1
+    let! v = identifier
+    let! _ = pstring "=" .>> spaces
+    let! left = pTerm 
+    let! _ = pstring "in" .>> spaces1
+    let! right = pTerm
+    return TmLet(v,left,right)}
+    
+
+and pLetRec = parse {
+    let! _ = pstring "letrec" .>> spaces1
+    let! v = identifier 
+    let! _ = pstring ":" .>> spaces
+    let! t = pType
+    let! _ = pstring  "=" .>> spaces 
+    let! t1 = pTerm 
+    let! _ = pstring "in" .>> spaces1
+    let! t2 = pTerm
+    return TmLet(v,TmFix(TmAbs(v,t,t1)),t2)}
+
+and pAppTerm = pPathTerm <|>
+    pAppTerm pPathTerm <|> 
+    pFix pPathTerm <|> 
+    pTimesFloat pPathTerm pPathTerm <|> 
+    pSucc pPathTerm <|> 
+    pPred pPathTerm <|> 
+    pIsZero pPathTerm
+
+and pAscribeTerm = pATerm "as" pType <|> pATerm
+
+and pPathTerm = pPathTerm "." identifier <|> pPathTerm "." pint32 <|> pAscribeTerm
+
+and pTermSeq = pTerm <|> pTerm ";" pTermSeq
+
+and pATerm = pParenTermSeq <|>
+    "inert" "[" pType "]" <|>
+    pTrue <|> pFalse <|>
+    "<" identifier "=" pTerm ">" "as" pType <|>
+    ideintifier <|>
+    pStringv <|>
+    "unit" <|>
+    "{" pFields "}" <|>
+    pfloat <|>
+    pint32
+
+and pLambda = parse {
+    let! _ = pstring "λ"
+    do! spaces
+    let! name = identifier
+    let! ctx = getUserState
+    do! spaces
+    let! _ = pstring ":"
+    do! spaces
+    let! typ = pType
+    do! updateUserState(addname name)
+    do! spaces
+    let! _ = pstring "."
+    do! spaces
+    let! term = pTerm
+    do! setUserState(ctx)
+    return TmAbs(name, typ, term)}
+    
+and pIf = parse{
+    let! _ = pstring "if" 
+    do! spaces
+    let! cond = pTerm
+    do! spaces
+    let! _ = pstring "then"
+    do! spaces
+    let! trueTerm = pTerm
+    do! spaces
+    let! _ = pstring "else"
+    do! spaces
+    let! falseTerm = pTerm
+    return TmIf(cond, trueTerm, falseTerm)}
+
+and pParen = parse{ 
+    let! _ = pstring "("
+    let! t = pTerm
+    let! _ = pstring ")"
+    do! spaces
+    return t}
+
+and pCases = chainr1 pCase (pstring "|" >>% fun x1 x2 -> x1::x2)
+
+and pCase = parse {
+    let! _ = pstring "<"
+    let! v1 = identifier 
+    let! _ = pstring "="
+    let! v2 = identifier
+    let! _ = pstring  ">" 
+    let! _ = pstring "==>"
+    let! t1 = pAppTerm
+    return (v1, (v2, t1))}
+
+and pFields = [] <|> pNEFields
+    
+and pNEFields = chainr1 pField (pstring "," >>% fun x1 x2 -> (x1, i)::(x2, i+1))
+
+and pField = parse{
+        let! f = identifier 
+        let! _ = pstring "="
+        let! t = pTerm
+        return (f,t)} <|>
+    parse{
+        let! t = pTerm
+        return (string_of_int i, t)}
